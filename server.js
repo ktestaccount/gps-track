dc7a0eaf9a89ca841a87eea3b01e509feddb8a9e
ktestaccount <khzxcvbnm1974@gmail.com>
2024-05-05 08:24:30 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { readFileSync } = require("fs");
+const bodyParser = require('body-parser');
+const webSocket = require("ws");
+const https = require("https");
+const app = express();
+const httpsPort = process.env.PORT || 8000;
+const datas = [];
+app.use(bodyParser.json());
+
+app.get("/", (req, resp) => {
+    resp.status(200).send(readFileSync("./index.html", {encoding: "utf-8"}));
+});
+app.post("/save-data", (req, resp) => {
+    const { speed, angle, distance, timestamp,latitude,longitude } = req.body;
+    datas.push({ speed, angle, distance, timestamp,latitude,longitude });
+    console.log(speed, angle, distance, timestamp, latitude,longitude);
+    resp.send({ success: true });
+});
+app.get("/data", (req, resp) => {
+    resp.send(datas);
+});
+app.listen(httpsPort, () => {
+    console.log(`server running at port ${httpsPort}`);
+})
